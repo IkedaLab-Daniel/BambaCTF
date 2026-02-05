77e632a8ce1af39729ac9c0e0a16a83ae691b3f2
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-05 12:20:00 +0800

    Add Home component with styling and problem set layout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <div className="problem-set">
+        <div className="problem1">
+            <span className="label">Level 1</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem1">
+            <span className="label">Level 2</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem1">
+            <span className="label">Level 3</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem1">
+            <span className="label">Level 4</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+      </div>
     </div>
   )
 }
