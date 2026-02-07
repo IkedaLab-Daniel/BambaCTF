92834dadb54e12fda8a8ef259c80c16aa58d4972
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-07 23:35:58 +0800

    Refactor Home component to integrate Problems component and remove inline problem definitions; add Problems.css for styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,35 +1,12 @@
 import Categories from './Categories'
 import './Home.css'
+import Problems from './Problems'
 
 const Home = () => {
   return (
     <div className='home'>
       <Categories />
-      <div className="problem-set">
-        <div className="problem1">
-            <span className="label">Level 1</span>
-            <span className="title">HTML Inspector</span>
-            <button>Start</button>
-        </div>
-
-        <div className="problem1">
-            <span className="label">Level 2</span>
-            <span className="title">HTML Inspector</span>
-            <button>Start</button>
-        </div>
-
-        <div className="problem1">
-            <span className="label">Level 3</span>
-            <span className="title">HTML Inspector</span>
-            <button>Start</button>
-        </div>
-
-        <div className="problem1">
-            <span className="label">Level 4</span>
-            <span className="title">HTML Inspector</span>
-            <button>Start</button>
-        </div>
-      </div>
+      <Problems />
     </div>
   )
 }
---
+++ client/src/components/Problems.jsx
@@ -0,0 +1,33 @@
+import "./Problems.css";
+
+const Problems = () => {
+  return (
+    <div className="problem-set">
+        <div className="problem-card">
+            <span className="label">Level 1</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem-card">
+            <span className="label">Level 2</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem-card">
+            <span className="label">Level 3</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+
+        <div className="problem-card">
+            <span className="label">Level 4</span>
+            <span className="title">HTML Inspector</span>
+            <button>Start</button>
+        </div>
+    </div>
+  )
+}
+
+export default Problems
